65ad1c78560b143a9214f9eb2b534586bf70589b
Danny Pifer <piferdg@gmail.com>
2018-08-02 18:21:29 -0600

    Edited knexfile to fix production

--- knexfile.js
+++ knexfile.js
@@ -8,11 +8,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    }
-  }
+    client: "pg",
+    connection: process.env.DATABASE_URL
+}
+
 }
